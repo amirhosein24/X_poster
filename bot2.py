1a16ce569b07ef5bd3babbaa91c7c3d825215c45
amirhosein24 <am.heidari24@gmail.com>
2024-01-06 16:40:08 +0330

    update

--- bot2.py
+++ bot2.py
@@ -5,42 +5,28 @@ import json
 with open("creds.json") as file:
     data = json.load(file)
 
-# consumer_key = data["apikey" ]
-# consumer_secret = data["apikeysecret"]
-# access_token = data["accesstoken"]
-# access_token_secret = data["accesstokensecret"]
-# In your terminal please set your environment variables by running the following lines of code.
-# export 'CONSUMER_KEY'='<your_consumer_key>'
-# export 'CONSUMER_SECRET'='<your_consumer_secret>'
-
 consumer_key = data["apikey" ]
 consumer_secret = data["apikeysecret"]
 
-# Be sure to add replace the text of the with the text you wish to Tweet. You can also add parameters to post polls, quote Tweets, Tweet with reply settings, and Tweet to Super Followers in addition to other features.
 payload = {"text": "Hello world!"}
 
-# Get request token
 request_token_url = "https://api.twitter.com/oauth/request_token?oauth_callback=oob&x_auth_access_type=write"
 oauth = OAuth1Session(consumer_key, client_secret=consumer_secret)
 
 try:
     fetch_response = oauth.fetch_request_token(request_token_url)
 except ValueError:
-    print(
-        "There may have been an issue with the consumer_key or consumer_secret you entered."
-    )
+    print("There may have been an issue with the consumer_key or consumer_secret you entered.")
 
 resource_owner_key = fetch_response.get("oauth_token")
 resource_owner_secret = fetch_response.get("oauth_token_secret")
 print("Got OAuth token: %s" % resource_owner_key)
 
-# Get authorization
 base_authorization_url = "https://api.twitter.com/oauth/authorize"
 authorization_url = oauth.authorization_url(base_authorization_url)
 print("Please go here and authorize: %s" % authorization_url)
 verifier = input("Paste the PIN here: ")
 
-# Get the access token
 access_token_url = "https://api.twitter.com/oauth/access_token"
 oauth = OAuth1Session(
     consumer_key,
@@ -54,7 +40,6 @@ oauth_tokens = oauth.fetch_access_token(access_token_url)
 access_token = oauth_tokens["oauth_token"]
 access_token_secret = oauth_tokens["oauth_token_secret"]
 
-# Make the request
 oauth = OAuth1Session(
     consumer_key,
     client_secret=consumer_secret,
@@ -69,12 +54,9 @@ response = oauth.post(
 )
 
 if response.status_code != 201:
-    raise Exception(
-        "Request returned an error: {} {}".format(response.status_code, response.text)
-    )
+    raise Exception("Request returned an error: {} {}".format(response.status_code, response.text))
 
 print("Response code: {}".format(response.status_code))
 
-# Saving the response as JSON
 json_response = response.json()
 print(json.dumps(json_response, indent=4, sort_keys=True))
