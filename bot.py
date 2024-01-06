685b8dc6f4b974e26e367f194608209b5563bbaf
amirhosein24 <am.heidari24@gmail.com>
2024-01-06 16:06:25 +0330

    update

--- bot.py
+++ bot.py
@@ -9,30 +9,30 @@ consumer_secret = data["apikeysecret"]
 access_token = data["accesstoken"]
 access_token_secret = data["accesstokensecret"]
 
-# auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-# auth.set_access_token(access_token, access_token_secret)
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
 
-# api = tweepy.API(auth)
+api = tweepy.API(auth)
 
-# tweet_text = "Hello, Twitter!"
-# try:
-#     api.update_status(tweet_text)
+tweet_text = "Hello, Twitter!"
+try:
+    api.update_status(tweet_text)
 
-#     print("Tweet posted successfully!")
+    print("Tweet posted successfully!")
 
-# except Exception as e:
-#     print(e)
+except Exception as e:
+    print(e)
 
 
 
-client = tweepy.Client(
-    consumer_key=consumer_key,
-    consumer_secret=consumer_secret,
-    access_token=access_token,
-    access_token_secret=access_token_secret
-)
+# client = tweepy.Client(
+#     consumer_key=consumer_key,
+#     consumer_secret=consumer_secret,
+#     access_token=access_token,
+#     access_token_secret=access_token_secret
+# )
 
-# Post Tweet
-message = " MESSAGE "
-client.create_tweet(text=message)
-print("Tweeted!")
+# # Post Tweet
+# message = " MESSAGE "
+# client.create_tweet(text=message)
+# print("Tweeted!")
